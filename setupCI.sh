29e871198e105d0e7414365cc9d6c4188b483cb3
Jim Shingler <ShinglerJim@gmail.com>
2016-09-16 14:44:40 -0400

    added rebase test

--- setupCI.sh
+++ setupCI.sh
@@ -16,9 +16,9 @@ target=$1
 echo ""
 echo "Setup ${branch}_CI on target: ${target}"
 echo "========================================"
-fly -t ${target} set-pipeline -p ${branch}_CI -c ci/pipeline.yml -l credentials.yml
+fly -t ${target} set-pipeline --pipeline ${branch}_CI -c ci/pipeline.yml -l credentials.yml
 
 echo ""
 echo "Setup ${branch}_Gated on target: ${target}"
 echo "========================================"
-fly -t ${target} set-pipeline -p ${branch}_Gated -c ci/pipeline-gated.yml -l credentials.yml
+fly -t ${target} set-pipeline --pipeline ${branch}_Gated -c ci/pipeline-gated.yml -l credentials.yml
