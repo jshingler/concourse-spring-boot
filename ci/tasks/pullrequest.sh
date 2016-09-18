90fdee5cdc89edc71a1ce27a89dddfd121002bbf
Jim Shingler <ShinglerJim@gmail.com>
2016-09-17 23:21:20 -0400

    add -i to get headers

--- ci/tasks/pullrequest.sh
+++ ci/tasks/pullrequest.sh
@@ -9,7 +9,13 @@ export TERM=${TERM:-dumb}
 # echo "2=$2"
 env
 
-curl -u "${username}:${password}" -H "Content-Type: application/json" -X POST -d '{"title": "'"${branch}"'", "body": "Gated Pull Request from CI", "head": "'"${branch}"'", "base": "'"${target}"'"}' "${uri}/pulls"
+# branch=feature2
+# username=shinglerjim@gmail.com
+# target=master
+# password=s4md0g12
+# uri=https://github.com/jshingler/concourse-spring-boot
+
+curl -u "${username}:${password}" -H "Content-Type: application/json" -X POST -d '{"title": "'"${branch}"'", "body": "Gated Pull Request from CI", "head": "'"${branch}"'", "base": "'"${target}"'"}' -i "${uri}/pulls"
 
 echo "=============================="
 echo "Issued Pull request from ${branch} to ${target}"
