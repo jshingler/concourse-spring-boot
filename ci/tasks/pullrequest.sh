2c0f0ad4e5a3d895fe29b745441a317912d71efe
Jim Shingler <ShinglerJim@gmail.com>
2016-09-17 23:43:54 -0400

    cleanup

--- ci/tasks/pullrequest.sh
+++ ci/tasks/pullrequest.sh
@@ -9,12 +9,6 @@ export TERM=${TERM:-dumb}
 # echo "2=$2"
 env
 
-# branch=feature2
-# username=shinglerjim@gmail.com
-# target=master
-# password=s4md0g12
-# uri=https://github.com/jshingler/concourse-spring-boot
-
 curl -u "${username}:${password}" -H "Content-Type: application/json" -X POST -d '{"title": "'"${branch}"'", "body": "Gated Pull Request from CI", "head": "'"${branch}"'", "base": "'"${target}"'"}' -i "${uri}/pulls"
 
 echo "=============================="
