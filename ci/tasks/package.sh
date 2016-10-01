d1d3cec7c1b43348e2a5d1812433d2539601347b
Jim Shingler <ShinglerJim@gmail.com>
2016-09-30 23:05:02 -0400

    Use local IP

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -9,7 +9,7 @@ echo "Build and Publish to Maven Repo"
 pwd
 cd source-code
 pwd
-./gradlew -PrepoUrl=${MAVEN_REPO} assemble publish
+./gradlew -PrepoUrl=http://192.168.1.178:8081 assemble publish
 
 echo "Move artifacts to output area"
 cp build/libs/concourse-spring-boot*.jar ../build
