aae4d9098db02804ad4a5c6c36c82357249dffac
James Shingler <James.Shingler@CardinalHealth.com>
2016-12-12 13:13:47 -0500

    Temporarily disable publish to nexus

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -9,6 +9,8 @@ echo "Build and Publish to Maven Repo: ${MAVEN_REPO}"
 pwd
 cd source-code
 pwd
+# JLS Temporarily disable publish to nexus
+#./gradlew -PrepoUrl=${MAVEN_REPO} assemble publish
 ./gradlew -PrepoUrl=${MAVEN_REPO} assemble publish
 
 echo "Move artifacts to output area"
